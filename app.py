3f71239e2cf262221e4373909ffb1e542ded81b2
puff <37280121+crempuff@users.noreply.github.com>
2023-06-17 22:42:39 +0000

    Deleted CSS, Added Bootstrap

--- app.py
+++ app.py
@@ -5,3 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return render_template("index.html", title="Hello")
+
+@app.route("/hello/<name>")
+def hello(name):
+    return "Hi " + name
