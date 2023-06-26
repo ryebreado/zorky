f98a5e1b78c4f613cd13612fe61c6dc41b7decc1
puff <37280121+crempuff@users.noreply.github.com>
2023-06-26 01:56:18 +0000

    added cookies and fun stuff

--- app.py
+++ app.py
@@ -1,13 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, make_response, redirect, url_for
 from . import rooms
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return render_template("index.html", title="Hello")
+def home():
+    name = request.cookies.get("userName")
+    return render_template("index.html", title="Hello", name = name)
 
 @app.route("/room/<name>")
 def displayRoom(name):
     room = rooms.home[name]
-    return room.description
+    return render_template("room.html", description=room.description, title = name)
+
+@app.route('/setname', methods = ['POST', 'GET'])
+def setname():
+    if request.method == 'POST':
+        name = request.form['nm']
+   
+        resp = make_response(redirect(url_for("home")))
+        resp.set_cookie('userName', name)
+   
+        return resp
