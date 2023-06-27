3642c5fa42784dce069abb9fb0df0f0b9060baac
Rigel Cierniak <55332516+ryebreado@users.noreply.github.com>
2023-06-27 03:01:25 +0000

    room navigation

--- app.py
+++ app.py
@@ -3,22 +3,30 @@ from . import rooms
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def home():
     name = request.cookies.get("userName")
-    return render_template("index.html", title="Hello", name = name)
+    return render_template("index.html", 
+                           title="Hello", 
+                           name=name)
+
 
 @app.route("/room/<name>")
 def displayRoom(name):
     room = rooms.home[name]
-    return render_template("room.html", description=room.description, title = name)
+    return render_template("room.html", 
+                           description=room.description,
+                           paths=room.paths,
+                           title=name)
 
-@app.route('/setname', methods = ['POST', 'GET'])
+
+@app.route('/setname', methods=['POST', 'GET'])
 def setname():
     if request.method == 'POST':
         name = request.form['nm']
-   
+
         resp = make_response(redirect(url_for("home")))
         resp.set_cookie('userName', name)
-   
+
         return resp
--- rooms.py
+++ rooms.py
@@ -1,14 +1,14 @@
-outside = {"name": "outside", "paths": {"N": "entry room"}}
-outside["description"] = "you are outside. There is a house to the north. The door is slightly open."
-entryroom = {"name": "entry room", "paths": {
-    "S": "outside", "NE": "TV room", "W": "kitchen", "N": "stairs"}}
-entryroom["description"] = "you have entered the house. To the east is the living room, west to kitchen, north to stairway"
+outside = {"name": "outside", "paths": {"N": "entryroom"}}
+outside["description"] = "You are outside. There is a house to the north. The door is slightly open."
+entryroom = {"name": "entryroom", "paths": {
+    "S": "outside", "NE": "tvroom", "W": "kitchen", "N": "stairs"}}
+entryroom["description"] = "You have entered the house. To the east is the living room, west to kitchen, north to stairway"
 kitchen = {"name": "kitchen", "paths": {
-    "N": "bathroom", "NE": "hallway", "E": "entry room"}}
+    "N": "bathroom", "NE": "hallway", "E": "entryroom"}}
 kitchen["description"] = "You are in a kitchen. There is a refrigerator with a strange smell coming out of it. There is a counter and table."
-hallway = {"name": "hallway", "paths": {"E": "TV room", "SW": "kitchen"}}
+hallway = {"name": "hallway", "paths": {"E": "tvroom", "SW": "kitchen"}}
 hallway["description"] = "You are in a hallway. It is dark...."
-tvroom = {"name": "TV room", "paths": {"W": "hallway", "SW": "entry room"}}
+tvroom = {"name": "tvroom", "paths": {"W": "hallway", "SW": "entryroom"}}
 tvroom["description"] = "There is a big TV in the room. The screen has a large crack in it."
 
 house = {}
